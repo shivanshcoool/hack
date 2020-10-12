99b56a51b5e67f412a342b543b6612b1ad834232
Shivansh063 <31778127+Shivansh063@users.noreply.github.com>
2020-10-13 00:58:23 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,1 @@
-let http = require('http');
-
-http.createServer(function(req, resp){
-    resp.writeHead(404,{"Content-Type" : "text/html"});
-    resp.write("Hello World !!");
-    resp.end();
-}).listen(8080);
+function CalculateAge() ={}
